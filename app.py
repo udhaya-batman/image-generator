b6ce9a165d3e5b8fa4f206e3b17ceec4ef415b55
udhaya-batman <raahuludhaya6@gmail.com>
2025-04-15 22:29:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="AI Image Generator 🎨", layout="centered")
 st.title("🧠 AI Image Generator from Text")
 st.write("Enter a prompt, and get an AI-generated image!")
 
-openai.api_key = os.getenv(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)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 prompt = st.text_input("Enter image description:", placeholder="e.g. A cat wearing sunglasses on a beach")
 
